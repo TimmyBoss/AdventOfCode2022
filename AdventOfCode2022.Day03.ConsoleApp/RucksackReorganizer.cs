8c57ccde5f2804f4d5c79c8f6d0f36c47825a522
Tim Harte <tim@scorito.com>
2022-12-05 09:14:43 +0100

    Solved task 2 of day 3

--- AdventOfCode2022.Day03.ConsoleApp/RucksackReorganizer.cs
+++ AdventOfCode2022.Day03.ConsoleApp/RucksackReorganizer.cs
@@ -4,7 +4,8 @@ namespace AdventOfCode2022.Day03.ConsoleApp
 {
     internal class RucksackReorganizer : ITask
     {
-        List<string> _items;
+        List<string> _items = new List<string>();
+        List<List<string>> _itemGroups = new List<List<string>>();
 
         public long GetAnswer1()
         {
@@ -20,18 +21,37 @@ namespace AdventOfCode2022.Day03.ConsoleApp
 
         public long GetAnswer2()
         {
-            return 0;
+            var totalPriorities = 0;
+            var priorities = new Priorities();
+
+            foreach (var itemGroup in _itemGroups)
+            {
+                var recurringChar = GetRecurringCharInGroup(itemGroup);
+                totalPriorities += priorities[recurringChar];
+            }
+            return totalPriorities;
         }
 
         public void SetupPuzzleInput(List<string> puzzleInput)
         {
             _items = puzzleInput;
+            _itemGroups = GetItemGroups(puzzleInput);
+        }
+
+        public List<List<string>> GetItemGroups(List<string> puzzleInput)
+        {
+            var itemGroups = new List<List<string>>();
+
+            int i = 0;
+            int chunkSize = 3;
+            itemGroups = puzzleInput.GroupBy(s => i++ / chunkSize).Select(g => g.ToList()).ToList();
+            return itemGroups;
         }
 
         public int GetPriority(string item)
         {
-            var firstCompartment = item.Substring(0, (int)(item.Length / 2));
-            var lastCompartment = item.Substring((int)(item.Length / 2), (int)(item.Length / 2));
+            var firstCompartment = item.Substring(0, item.Length / 2);
+            var lastCompartment = item.Substring(item.Length / 2, item.Length / 2);
             var priorities = new Priorities();
 
             foreach (var character in firstCompartment)
@@ -42,5 +62,19 @@ namespace AdventOfCode2022.Day03.ConsoleApp
 
             return 0;
         }
+
+        public char GetRecurringCharInGroup(List<string> itemGroup)
+        {
+            var item = itemGroup[0];
+
+            foreach (var character in item)
+            {
+                if (itemGroup[1].Contains(character) && itemGroup[2].Contains(character))
+                    return character;
+            }
+
+            return '\0';
+        }
+
     }
 }
