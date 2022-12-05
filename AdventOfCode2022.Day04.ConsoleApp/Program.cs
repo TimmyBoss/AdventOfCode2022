41f3083c7504f3237d87b3cacef84faf9b43f7a7
Tim Harte <tim@scorito.com>
2022-12-05 12:54:12 +0100

    Solved Task1 of Day4

---
+++ AdventOfCode2022.Day04.ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2022.Day04.ConsoleApp;
+
+var task = new AdventOfCode2022.Shared.Task<CampCleaner>();
+task.ReadAnswers();
---
+++ AdventOfCode2022.Day04.ConsoleApp/SectionAssignment.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2022.Day04.ConsoleApp
+{
+    public class SectionAssignment
+    {
+        public SectionAssignment() { }
+
+
+        public int StartSection { get; set; }
+        public int EndSection { get; set; }
+    }
+}
