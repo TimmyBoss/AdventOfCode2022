4e824e19ab0e1abb7ebfb1c1613e4d030e24e442
Timmay-26 <ttjharte@ziggo.nl>
2022-12-06 09:03:44 +0100

    Day 5 in progress. Added Stack object.

--- AdventOfCode2022.Day05.ConsoleApp/SupplyStacker.cs
+++ AdventOfCode2022.Day05.ConsoleApp/SupplyStacker.cs
@@ -4,6 +4,8 @@ namespace AdventOfCode2022.Day05.ConsoleApp
 {
     internal class SupplyStacker : ITask
     {
+        private Stack _stack = new Stack();
+
         public long GetAnswer1()
         {
             return 0;
@@ -16,6 +18,21 @@ namespace AdventOfCode2022.Day05.ConsoleApp
 
         public void SetupPuzzleInput(List<string> puzzleInput)
         {
+            var _stack = GetStack(puzzleInput);
+        }
+
+        public Stack GetStack(List<string> puzzleInput)
+        {
+            var stack = new List<string>();
+            var i = 0;
+
+            while (!string.IsNullOrEmpty(puzzleInput[i]))
+            {
+                stack.Add(puzzleInput[i]);
+                i++;
+            }
+
+            return new Stack(stack);
         }
     }
 }
