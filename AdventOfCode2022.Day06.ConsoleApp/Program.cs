3e2f3b10a0fdd5ed295f7a124820575b778e4fff
Timmay-26 <ttjharte@ziggo.nl>
2022-12-10 14:04:21 +0100

    Fixed task 1 of day 6

---
+++ AdventOfCode2022.Day06.ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2022.Day06.ConsoleApp;
+
+var task = new AdventOfCode2022.Shared.Task<Tuner>();
+task.ReadAnswers();
---
+++ AdventOfCode2022.Day06.ConsoleApp/Tuner.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2022.Shared;
+
+namespace AdventOfCode2022.Day06.ConsoleApp
+{
+    internal class Tuner : ITask
+    {
+        private string _dataStreamBuffer = "";
+        private int sequenceAmount = 4;
+
+        public string GetAnswer1()
+        {
+            var amount = sequenceAmount;
+
+            for (int i = 0; i < _dataStreamBuffer.Length; i++)
+            {
+                var substring = _dataStreamBuffer.Substring(i, sequenceAmount);
+                var hasDuplicates = substring.GroupBy(c => c).Any(g => g.Count() > 1);
+
+                if (!hasDuplicates)
+                    break;
+
+                amount++;
+            }
+
+            return amount.ToString();
+        }
+
+        public string GetAnswer2()
+        {
+            return "";
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _dataStreamBuffer = puzzleInput[0];
+        }
+    }
+}
